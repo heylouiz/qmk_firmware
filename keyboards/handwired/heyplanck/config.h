cbfc977f0b1e81cbd56d2453651b081670b952e4
Luiz Silva <luizfrdasilva@gmail.com>
2018-06-30 21:51:33 -0300

    Remap arduino pins after resolder

--- keyboards/handwired/heyplanck/config.h
+++ keyboards/handwired/heyplanck/config.h
@@ -33,8 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 12
 
 /* Planck PCB default pin-out */
-#define MATRIX_ROW_PINS { B5, B4, B2, B6 }
-#define MATRIX_COL_PINS { D3, D2, F4, F5, F6, F7, B1, B3, D4, C6, D7, E6 }
+#define MATRIX_ROW_PINS { B4, B5, B2, B6 }
+#define MATRIX_COL_PINS { E6, D7, C6, D4, D2, D3, F4, F5, F6, F7, B1, B3 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
